8d5c28765d43c0796a57b84e4b1400730787278f
Harrisam <samantha.s.harris@googlemail.com>
2013-10-01 16:54:30 +0100

    final touches

--- javascript/lib/scorejQuery.js
+++ javascript/lib/scorejQuery.js
@@ -1,5 +1,17 @@
+function getRandomPick() {
+  var list = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+  var elemlength = list.length; //5
+
+  var randomnum = Math.floor(Math.random()*elemlength);
+  var randomitem = list[randomnum]; // 'scissors'
+
+  return randomitem;
+  // $(randomitem).css("display", "block");
+}
+
+
 $(document).ready(function() {
-  	var player1 = new Player('Alex');
+  	var player1 = new Player('Sam');
 	var player2 = new Player('Martin');
 	var game = new Game(player1, player2);
 	// var list = $("ul li").toArray();
@@ -9,7 +21,9 @@ $(document).ready(function() {
  // "$(randomitem).css("display", "block")"
 	$('.choices img').click(function() {
 		player1.picks($(this).data('name'));
-		player2.picks('rock');
+		var randpick = getRandomPick();
+		player2.picks(randpick);
+		console.log(randpick)
 
       	var winner = game.winner()
 
@@ -25,8 +39,6 @@ $(document).ready(function() {
 
 
 
-
-  
 
 
 
